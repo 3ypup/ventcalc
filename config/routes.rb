b9cd690a1ed102b694d3acd107471ad6309b1361
3ypup <posilay.pochashe@gmail.com>
2018-05-04 15:32:00 +0300

    index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users
 
-get '/' => "users#profile"
+get '/' => "users#index"
 
 end
