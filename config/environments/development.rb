7b7cd6b42225b7c7634f8e1fa023c8abb13b5a29
3ypup <posilay.pochashe@gmail.com>
2018-05-10 13:39:40 +0300

    devfix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.require_master_key = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -59,20 +60,20 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
- # config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => Rails.application.credentials.user_name
-   :password => Rails.application.credentials.email_pass
- }
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+  # :tls => true,
+  # :address => "smtp.gmail.com",
+  # :port => 587,
+  # :domain => "gmail.com",
+ #  :authentication => :login,
+ #  :user_name => Rails.application.credentials.user_name
+#   :password => Rails.application.credentials.email_pass
+# }
 
 end
