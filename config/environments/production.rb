00b38adc730c306356cc0246989f311ba8c9d862
3ypup <posilay.pochashe@gmail.com>
2018-05-10 15:14:55 +0300

    prod update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-#ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
 #ActionMailer::Base.smtp_settings = {
  #  :tls => true,
   # :address => "smtp.gmail.com",
