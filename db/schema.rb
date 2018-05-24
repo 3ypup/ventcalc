a79a1ef10ca66ae9d95296dd98028f154ac87e8c
3ypup <posilay.pochashe@gmail.com>
2018-05-24 13:02:32 +0300

    posts added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_093322) do
+ActiveRecord::Schema.define(version: 2018_05_24_094614) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "author"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
