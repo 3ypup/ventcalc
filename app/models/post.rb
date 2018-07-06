2a528b117b4a0aeb62daf0b61ad9215dabf97188
3ypup <posilay.pochashe@gmail.com>
2018-07-06 11:33:25 +0300

    some verstka

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
   validates :title, :length => {:minimum => 3, :maximum => 20} 
   validates :author, presence: true
   validates :content, presence: true
-  validates :content, :length => {:minimum => 10, :maximum => 200}
+  validates :content, :length => {:minimum => 10, :maximum => 2000}
   validates :image, presence: true
   validates_format_of :image,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
 
